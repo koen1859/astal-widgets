a14bfdf04a6ec09268fa5e0a62ced542b8274ab8
koen1859 <koenstevens1859@gmail.com>
2025-06-22 17:58:22 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    ags = {
+      url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ags,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+
+    agsPackages = with ags.packages.${system}; [
+      gjs
+      io
+      astal3
+      astal4
+      battery
+      hyprland
+      wireplumber
+      network
+      mpris
+      powerprofiles
+      tray
+      bluetooth
+    ];
+  in {
+    packages.${system}.default = ags.lib.bundle {
+      inherit pkgs;
+      name = "simple-bar";
+      src = ./.;
+      entry = "app.ts";
+      gtk4 = false;
+
+      extraPackages = agsPackages ++ [pkgs.gjs];
+    };
+
+    # Apps for easy running
+    apps.${system} = {
+      default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/simple-bar";
+      };
+    };
+
+    # Overlay for using in NixOS configurations
+    overlays.default = final: prev: {
+      simple-bar = self.packages.${system}.default;
+    };
+
+    devShells.${system}.default = pkgs.mkShell {
+      packages = agsPackages ++ [pkgs.gjs];
+    };
+  };
+}
